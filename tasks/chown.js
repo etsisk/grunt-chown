f12d3843f694c17312fed585778225cb7b9095a4
Eamon Sisk <eamonsisk@gmail.com>
2013-12-16 23:26:26 -0500

    Updated success message/fixed indentation

--- tasks/chown.js
+++ tasks/chown.js
@@ -16,22 +16,22 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('chown', 'A Grunt plugin that allows you to change the owner of a file.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({ uid: 32767, gid: 32767 }),
-		uid = options.uid,
-		gid = options.gid,
-		fs;
+    uid = options.uid,
+    gid = options.gid,
+    fs;
 	
-	if ( typeof uid !== 'number' ) {
-		grunt.fail.warn( 'Value of `uid` must be an unsigned integer.' );
-	}
-	
-	if ( typeof gid !== 'number' ) {
-		grunt.fail.warn( 'Value of `gid` must be an unsigned integer.' );
-	}
+    if ( typeof uid !== 'number' ) {
+      grunt.fail.warn( 'Value of `uid` must be an unsigned integer.' );
+    }
 
-	fs = require( 'fs' );
+    if ( typeof gid !== 'number' ) {
+      grunt.fail.warn( 'Value of `gid` must be an unsigned integer.' );
+    }
+
+    fs = require( 'fs' );
     // Iterate over all specified file groups.
     this.files.forEach( function(f) {
-      // Concat specified files.
+      // Update specified files.
       var src = f.src.filter( function(filepath) {
         // Warn on and remove invalid source files (if nonull was set).
         if ( !grunt.file.exists( filepath ) ) {
@@ -44,12 +44,10 @@ module.exports = function(grunt) {
                 grunt.fail.warn( e + ' on file "' + filepath + '".' );
             }
         }
-      });
 
-      // Print a success message.
-      grunt.log.ok( 'File "' + f.src + '" uid set to "' + uid + '".' );
-      grunt.log.ok( 'File "' + f.src + '" gid set to "' + gid + '".' );
+        // Print a success message.
+        grunt.log.write( filepath + ' ' ).ok( 'uid: ' + uid + ', gid: ' + gid );
+      });
     });
   });
-
 };
